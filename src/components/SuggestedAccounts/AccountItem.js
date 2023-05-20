5e7c1c43e7da116a808c136892b71300abea2271
Vi Le <vi.le22@student.passerellesnumeriques.org>
2023-05-20 19:56:33 +0700

    UI SideBar

---
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -0,0 +1,47 @@
+import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountPreview from './AccountPreview/AccountPreview';
+import styles from './SuggestedAccounts.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    const renderPreview = (props) => {
+        return (
+            <div tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview />
+                </PopperWrapper>
+            </div>
+        );
+    };
+
+    return (
+        <div>
+            <Tippy interactive delay={[800, 0]} offset={[-20, 0]} placement="bottom" render={renderPreview}>
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/3d5885b54ef66a166ecf5d7e6e0e4999~c5_100x100.jpeg?x-expires=1684677600&x-signature=uiHcxegQxRYxTfE8zFo%2BJ7tqeXY%3D"
+                        alt=""
+                    />
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>quocnguyenphu</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>Quốc Nguyễn Phú</p>
+                    </div>
+                </div>
+            </Tippy>
+        </div>
+    );
+}
+
+AccountItem.propTypes = {};
+
+export default AccountItem;
